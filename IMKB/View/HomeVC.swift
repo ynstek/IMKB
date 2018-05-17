b52381e5711cd6b810fab3125a8b75e81ed48026
Yunus Tek <ynstek@hotmail.com>
2018-05-18 00:02:07 +0300

    test soap data.

--- IMKB/View/HomeVC.swift
+++ IMKB/View/HomeVC.swift
@@ -7,28 +7,25 @@
 //
 
 import UIKit
+import Alamofire
+import SWXMLHash
+import StringExtensionHTML
+import AEXML
+
+struct Country {
+    var name:String = ""
+}
 
 class HomeVC: UIViewController, XMLParserDelegate {
-
-    var parser = XMLParser()
-    var data = NSMutableArray()
+    
+    var tableData = [Country]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
-    }
-
-    @IBAction func btnGet(_ sender: Any) {
-        parser = XMLParser(contentsOf: URL(string: "http://mobileexam.veripark.com/mobileforeks/service.asmx")!)!
-        parser.delegate = self
-        parser.parse()
-    }
-    
-    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-        
-        print(elementName)
-        
+        self.getCountries { (result) in
+            print(result)
+            self.tableData = result
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -36,15 +33,47 @@ class HomeVC: UIViewController, XMLParserDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func getCountries(completion: @escaping (_ result: [Country]) -> Void) -> Void {
+        
+        var result = [Country]()
+        let soapRequest = AEXMLDocument()
+        let envelopeAttributes = ["xmlns:SOAP-ENV" : "http://schemas.xmlsoap.org/soap/envelope/", "xmlns:ns1" : "http://www.webserviceX.NET"]
+        let envelope = soapRequest.addChild(name: "SOAP-ENV:Envelope", attributes: envelopeAttributes)
+        let body = envelope.addChild(name: "SOAP-ENV:Body")
+        body.addChild(name: "ns1:GetCountries")
+        
+        let soapLenth = String(soapRequest.xml.count)
+        let theURL = URL(string: "http://www.webservicex.net/country.asmx")
+        
+        var mutableR = URLRequest(url: theURL!)
+        mutableR.addValue("text/xml; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        mutableR.addValue("text/html; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        mutableR.addValue(soapLenth, forHTTPHeaderField: "Content-Length")
+        mutableR.httpMethod = "POST"
+        mutableR.httpBody = soapRequest.xml.data(using: String.Encoding.utf8)
+        
+        Alamofire.request(mutableR)
+            .responseString { response in
+                if let xmlString = response.result.value {
+                    let xml = SWXMLHash.parse(xmlString)
+                    let body =  xml["soap:Envelope"]["soap:Body"]
+                    if let countriesElement = body["GetCountriesResponse"]["GetCountriesResult"].element {
+                        let getCountriesResult = countriesElement.text
+                        let xmlInner = SWXMLHash.parse(getCountriesResult.stringByDecodingHTMLEntities)
+                        for element in xmlInner["NewDataSet"]["Table"].all {
+                            if let nameElement = element["Name"].element {
+                                var countryStruct = Country()
+                                countryStruct.name = nameElement.text
+                                result.append(countryStruct)
+                            }
+                        }
+                    }
+                    completion(result)
+                }else{
+                    print("error fetching XML")
+                }
+        }
     }
-    */
+
 
 }
